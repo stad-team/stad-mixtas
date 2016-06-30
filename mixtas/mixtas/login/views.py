25e236f8a6fe92b04dae5b8ffa77655dc6b3eb68
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-06-30 18:51:34 -0500

    login: api updated

--- mixtas/mixtas/login/views.py
+++ mixtas/mixtas/login/views.py
@@ -6,7 +6,7 @@ STAD Team
 from __future__ import unicode_literals
 
 from rest_framework.viewsets import GenericViewSet
-from rest_framework.mixins import ListModelMixin
+from rest_framework.mixins import RetrieveModelMixin
 
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate
@@ -14,14 +14,8 @@ from django.contrib.auth import authenticate
 from .serializers import LoginSerializer
 
 
-class LoginRetrieveView(ListModelMixin, GenericViewSet):
+class LoginRetrieveView(RetrieveModelMixin, GenericViewSet):
     model = User
     queryset = User.objects.all()
     serializer_class = LoginSerializer
-
-    def get_queryset(self):
-        user_name = self.request.query_params['name']
-        user_password = self.request.query_params['password']
-
-        # user = authenticate(username=user_name, password=user_password)
-        return User.objects.filter(username=user_name)
+    lookup_field = 'username'
