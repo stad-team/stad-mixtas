2f8e0013f717cc08371bf1296b259af1ab75e514
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-07-01 19:50:21 -0500

    app menu added

---
+++ mixtas/mixtas/menu/assets/js/components/menu.jsx
@@ -0,0 +1,52 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Menu extends React.Component {
+
+	constructor(props) {
+		super(props);
+
+		window.resizeTo(screen.availWidth, screen.availHeight);
+
+		window.moveTo(0,0);
+	}
+
+	render() {
+		const { user, superUser } = this.props;
+
+		let crearUsuarios = undefined;
+		if (superUser === 'True') {
+			crearUsuarios = (
+				<div>
+					Creación de Usuarios
+				</div>
+			);
+		}
+
+		if (user === 'AnonymousUser') {
+			window.location = '/auth/login';
+			return <div> Acceso Denegado </div>;
+		} else {
+			return(
+				<div>
+					<h1>
+						Bienvenido a Restaurante Mixtas
+					</h1>
+					<div>
+						Hola { user }
+						{ crearUsuarios }
+					</div>
+				</div>
+			);
+		}
+	}
+}
+
+const element = document.getElementById('react-menu');
+const user = element.getAttribute('data-user');
+const superUser = element.getAttribute('super-user');
+
+ReactDOM.render(
+	<Menu user={ user } superUser={ superUser } />,
+	document.getElementById('react-menu')
+);
