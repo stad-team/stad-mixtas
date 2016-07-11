6eaa4d2406d46ac5ef83b5c5aa5865f9a44f72f9
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-07-11 18:34:37 -0500

    usuarios: urls actualizado

--- mixtas/mixtas/usuarios/urls.py
+++ mixtas/mixtas/usuarios/urls.py
@@ -9,14 +9,14 @@ from django.conf.urls import patterns, url, include
 from django.views.generic import TemplateView
 
 from rest_framework import routers
-# from .views import LoginRetrieveView
+from .views import CrearUsuariosView
 
 # Routers provide an easy way of automatically determining the URL conf.
-# router = routers.SimpleRouter()
-# router.register(r'users', LoginRetrieveView)
+router = routers.SimpleRouter()
+router.register(r'usuarios', CrearUsuariosView)
 
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='usuarios/usuarios.html')),
-    # url(r'^api/', include(router.urls, namespace='api')),
+    url(r'^api/', include(router.urls, namespace='api')),
 )
