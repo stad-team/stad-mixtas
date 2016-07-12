817228497b415e73c0e58d0c65ca936828ac069f
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-07-12 18:18:27 -0500

    usuarios: SerializadorUsuarios actualizado, metodo create implementado

--- mixtas/mixtas/usuarios/serializers.py
+++ mixtas/mixtas/usuarios/serializers.py
@@ -13,3 +13,11 @@ from rest_framework import serializers
 class SerializadorUsuarios(serializers.ModelSerializer):
     class Meta:
         model = User
+
+    def create(self, validated_data):
+        password = validated_data.pop('password', None)
+        instance = self.Meta.model(**validated_data)
+        if password is not None:
+            instance.set_password(password)
+        instance.save()
+        return instance
