71dfaed9354fd9ec476ae2f67cc415591343ad48
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-07-25 15:54:37 -0500

    wsgi: updated to work with apache

--- mixtas/mixtas/wsgi.py
+++ mixtas/mixtas/wsgi.py
@@ -8,9 +8,13 @@ https://docs.djangoproject.com/en/1.8/howto/deployment/wsgi/
 """
 
 import os
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+sys.path.insert(0, BASE_DIR)
+sys.path.insert(0, os.path.join(BASE_DIR, 'mixtas'))
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "mixtas.settings")
 
 application = get_wsgi_application()
