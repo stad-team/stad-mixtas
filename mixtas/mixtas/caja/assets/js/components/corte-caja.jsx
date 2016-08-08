0960959e9f453c72de9068036234e5a192a76d16
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-08-07 23:54:47 -0500

    caja: corte-caja added

---
+++ mixtas/mixtas/caja/assets/js/components/corte-caja.jsx
@@ -0,0 +1,142 @@
+import React from 'react';
+import ReactDom from 'react-dom';
+
+import { connect, Provider } from 'react-redux';
+import { createStore, applyMiddleware, combineReducers } from 'redux';
+import promise from 'redux-promise';
+import thunkMiddleware from 'redux-thunk';
+
+import { isEmpty, remove } from 'lodash';
+
+//     ___   ____________________  _   _______
+//    /   | / ____/_  __/  _/ __ \/ | / / ___/
+//   / /| |/ /     / /  / // / / /  |/ /\__ \
+//  / ___ / /___  / / _/ // /_/ / /|  /___/ /
+// /_/  |_\____/ /_/ /___/\____/_/ |_//____/
+
+import axios from 'axios';
+
+const OBTENER_FOLIOS = 'OBTENER_FOLIOS';
+
+const actionObtenerFolios = () => {
+	const today = new Date();
+	const dd = today.getDate();
+	const locale = 'en-us';
+	const mm = today.toLocaleString(locale, {month: 'short'});
+	const yyyy = today.getFullYear();
+
+	const respuesta = axios.get(
+		`http://mixtas-costeno/pedidos/api/folio/?date=${ mm }-${ dd }-${ yyyy }`,
+		{
+			headers: {
+				'X-CSRFToken': tokenCSRF
+			}
+		}
+	);
+
+	return {
+		type: OBTENER_FOLIOS,
+		payload: respuesta
+	};
+};
+
+////////////////////////////////////////////////
+
+
+//     ____  __________  __  __________________  _____
+//    / __ \/ ____/ __ \/ / / / ____/ ____/ __ \/ ___/
+//   / /_/ / __/ / / / / / / / /   / __/ / /_/ /\__ \
+//  / _, _/ /___/ /_/ / /_/ / /___/ /___/ _, _/___/ /
+// /_/ |_/_____/_____/\____/\____/_____/_/ |_|/____/
+
+const reductorObtenerFolios = (state=[], action) => {
+	switch(action.type){
+	case OBTENER_FOLIOS:
+		return Object.assign([], state, action.payload.data);
+	default:
+		return state;
+	}
+};
+
+////////////////////////////////////////////////
+
+
+class CorteCaja extends React.Component {
+	componentWillMount() {
+		const { dispatch } = this.props;
+
+		dispatch(actionObtenerFolios());
+	}
+
+	render() {
+		const { folios } = this.props;
+
+		let listadoFolios;
+		let totalDia = 0;
+
+		if (!isEmpty(folios)) {
+			listadoFolios = folios.map(folio => {
+				totalDia += folio.total;
+				const date = new Date(folio.fecha);
+				return (
+					<tr>
+						<td> { folio.id } </td>
+						<td> { folio.nombreMesero } </td>
+				     	<td> { date.getDate() }/{ date.toLocaleString('es', {month: 'long'}) }/{ date.getFullYear() } - { date.toLocaleTimeString() } </td>
+				     	<td> { folio.total } </td>
+					</tr>
+				);
+			});
+		}
+
+		return (
+			<div className='container'>
+				<div className='col-md-12'>
+					<div className='my-users'>
+						<h3>Corte Diario</h3>
+					 	<table className='table table-hover'>
+							<thead>
+							    <tr>
+							    	<th>Folio</th>
+							    	<th>Mesero</th>
+							    	<th>Fecha</th>
+							    	<th>Total</th>
+							    </tr>
+							</thead>
+							<tbody key='orden-caja-1' className='orden-caja' >
+								{ listadoFolios }
+				    		</tbody>
+						</table>
+					</div>
+					<h3> Total Dia </h3>
+					<h3 className='total-dia'> { totalDia }</h3>
+				</div>
+			</div>
+		);
+	}
+}
+
+
+const CorteCajaConnect = connect(store => ({folios : store.listadoFolios }))(CorteCaja);
+
+const reducer = combineReducers({
+	listadoFolios: reductorObtenerFolios
+});
+
+const store = createStore(
+	reducer, applyMiddleware(thunkMiddleware, promise)
+);
+
+const element = document.getElementById('react-corte-caja');
+const tokenCSRF = element.getAttribute('data-token');
+
+
+ReactDom.render(
+	<Provider store={ store }>
+		<CorteCajaConnect />
+	</Provider>,
+	element
+);
+
+
+
