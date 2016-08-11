9559837af80370ee46bde076fc28a4124f5bec5d
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-08-11 14:08:22 -0500

    usuarios: SerializadorUsuarios updated, update method implemented to save new passwords

--- mixtas/mixtas/usuarios/serializers.py
+++ mixtas/mixtas/usuarios/serializers.py
@@ -21,3 +21,13 @@ class SerializadorUsuarios(serializers.ModelSerializer):
             instance.set_password(password)
         instance.save()
         return instance
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password != instance.password:
+            instance.set_password(password)
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
