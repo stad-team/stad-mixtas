d1a3ea02f50c6cd14686ed76198879e89dae6e34
Andrés Rivera Ayala <www.bloody.com@hotmail.com>
2016-08-11 19:12:58 -0500

    caja: urls updated, detalle-caja url fixed

--- mixtas/mixtas/caja/urls.py
+++ mixtas/mixtas/caja/urls.py
@@ -10,6 +10,6 @@ from django.views.generic import TemplateView
 
 urlpatterns = patterns('',
     url(r'^$', TemplateView.as_view(template_name='caja/caja.html')),
-    url(r'^detalle-caja/(?P<id>[0-9]*)/(?P<locacion>[A-Z,a-z]*)/(?P<idOrdenMesa>[0-9]*)/$', TemplateView.as_view(template_name='caja/detalle-caja.html')),
+    url(r'^detalle-caja/(?P<id>[0-9]*)/(?P<locacion>[A-Z,a-z,0-9]*)/(?P<idOrdenMesa>[0-9]*)/$', TemplateView.as_view(template_name='caja/detalle-caja.html')),
     url(r'^corte-caja/$', TemplateView.as_view(template_name='caja/corte-caja.html')),
 )
